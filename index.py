c6c7e73b60e9e6da8b947f768963fcaef9e659c8
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-10-22 21:26:45 +0300

    final commit:

---
+++ index.py
@@ -0,0 +1,14 @@
+print("Hello World")
+
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+sns.set()
+from sklearn import preprocessing
+
+
+data = pd.read_csv(r'D:\Tech\Tutorials\Business Analytcis\Customer Segmentation\7 [ DevCourseWeb.com ] Udemy - Customer Segmentation Analytics Masterclass 2023\code\source files')
+df.readgead()
